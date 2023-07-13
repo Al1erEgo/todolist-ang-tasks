f887c322d93638700f688fe2719006a8bed49b3b
Al1erEgo <al1erego@gmail.com>
2023-07-13 17:20:51 +0200

    feat: add logs in to tasks.service

--- src/app/todos/services/tasks.service.ts
+++ src/app/todos/services/tasks.service.ts
@@ -8,26 +8,39 @@ import {
   UpdateTaskRequest,
 } from 'src/app/todos/models/tasks.models'
 import { environment } from 'src/environments/environment'
-import { map } from 'rxjs/operators'
+import {map, tap} from 'rxjs/operators'
 import { CommonResponseType } from 'src/app/core/models/core.models'
+import {LoggerService} from "../../shared/services/logger.service";
 
 @Injectable({
   providedIn: 'root',
 })
 export class TasksService {
   tasks$ = new BehaviorSubject<DomainTask>({})
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private logger: LoggerService) {
+    this.logger.info('TasksService initialised', 'TasksService')
+  }
   getTasks(todoId: string) {
+    this.logger.info('Tasks data requested for todo(id):', 'TasksService', todoId);
     this.http
       .get<GetTasksResponse>(`${environment.baseUrl}/todo-lists/${todoId}/tasks`)
-      .pipe(map(res => res.items))
+      .pipe(
+        map(res => res.items),
+        tap(res => {
+        this.logger.info('Tasks data received:', 'TasksService', res);
+      }))
       .subscribe((res: Task[]) => {
         const stateTasks = this.tasks$.getValue()
         stateTasks[todoId] = res
         this.tasks$.next(stateTasks)
-      })
+        this.logger.info('Tasks data updated in tasks$ subject:', 'TasksService', res);
+      },
+        error => {
+          this.logger.error('Error occurred while fetching tasks:', 'TasksService', error);
+        })
   }
   addTask(todoId: string, title: string) {
+    this.logger.info('Add task request initialised', 'TasksService');
     this.http
       .post<CommonResponseType<{ item: Task }>>(
         `${environment.baseUrl}/todo-lists/${todoId}/tasks`,
@@ -40,13 +53,21 @@ export class TasksService {
           const newTasks = [newTask, ...stateTasks[todoId]]
           stateTasks[todoId] = newTasks
           return stateTasks
+        }),
+        tap(() => {
+          this.logger.info('Task added', 'TasksService', title);
         })
       )
       .subscribe(res => {
         this.tasks$.next(res)
-      })
+        this.logger.info('Tasks data updated in tasks$ subject:', 'TasksService', res);
+      },
+        error => {
+          this.logger.error('Error occurred while adding task:', 'TasksService', error);
+        })
   }
   deleteTask(todoId: string, taskId: string) {
+    this.logger.info('Delete task request initialised, taskId:', 'TasksService', taskId);
     this.http
       .delete<CommonResponseType>(`${environment.baseUrl}/todo-lists/${todoId}/tasks/${taskId}`)
       .pipe(
@@ -55,14 +76,21 @@ export class TasksService {
           const taskForTodo = stateTasks[todoId]
           stateTasks[todoId] = taskForTodo.filter(({ id }) => id !== taskId)
           return stateTasks
+        }),
+        tap(() => {
+          this.logger.info('Task deleted', 'TasksService');
         })
       )
       .subscribe(res => {
         this.tasks$.next(res)
+        this.logger.info('Tasks data updated in tasks$ subject:', 'TasksService', res);
+      }, error => {
+        this.logger.error('Error occurred while deleting task:', 'TasksService', error);
       })
   }
 
   updateTask(todoId: string, taskId: string, newTask: UpdateTaskRequest) {
+    this.logger.info('Update task request initialised', 'TasksService', newTask);
     this.http
       .put<CommonResponseType<{ item: Task }>>(
         `${environment.baseUrl}/todo-lists/${todoId}/tasks/${taskId}`,
@@ -77,10 +105,17 @@ export class TasksService {
           )
           stateTasks[todoId] = newTasks
           return stateTasks
+        }),
+        tap(() => {
+          this.logger.info('Task updated', 'TasksService');
         })
       )
       .subscribe(res => {
         this.tasks$.next(res)
-      })
+        this.logger.info('Tasks data updated in tasks$ subject:', 'TasksService', res);
+      },
+    error => {
+      this.logger.error('Error occurred while updating task:', 'TasksService', error);
+    })
   }
 }
