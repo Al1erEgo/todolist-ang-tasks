02273047a3d43d610f78384571acd47ce8d7422f
Al1erEgo <al1erego@gmail.com>
2023-07-13 15:14:43 +0200

    feat: add logs in to notify.component

--- src/app/shared/components/notify/notify.component.ts
+++ src/app/shared/components/notify/notify.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core'
 import { NotificationService } from 'src/app/core/services/notification.service'
 import { Observable } from 'rxjs'
 import { Notify } from 'src/app/core/models/notify.models'
+import {LoggerService} from "../../services/logger.service";
 
 @Component({
   selector: 'tl-notify',
@@ -11,14 +12,18 @@ import { Notify } from 'src/app/core/models/notify.models'
 export class NotifyComponent implements OnInit {
   notify$?: Observable<Notify | null>
 
-  constructor(private notificationService: NotificationService) {}
+  constructor(private notificationService: NotificationService, private logger: LoggerService) {
+    this.logger.info('Notification component initialised', 'NotificationComponent')
+  }
 
   ngOnInit(): void {
     //subscribe
     this.notify$ = this.notificationService.notify$
+    this.logger.info('Notification component subscribed on notification messages', 'NotificationComponent')
   }
 
   closeNotification() {
+    this.logger.info('Notification close', 'NotificationComponent')
     this.notificationService.clear()
   }
 }
