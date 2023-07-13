439cd61f41f415db007a838f4adffb991d102926
Al1erEgo <al1erego@gmail.com>
2023-07-13 15:43:32 +0200

    feat: add logs in to notification.service

--- src/app/core/services/notification.service.ts
+++ src/app/core/services/notification.service.ts
@@ -1,20 +1,28 @@
 import { Injectable } from '@angular/core'
 import { BehaviorSubject } from 'rxjs'
 import { Notify } from 'src/app/core/models/notify.models'
+import {LoggerService} from "../../shared/services/logger.service";
 
 @Injectable()
 export class NotificationService {
   notify$ = new BehaviorSubject<Notify | null>(null)
 
+  constructor(private logger: LoggerService) {
+    this.logger.info('NotificationService initialised', 'NotificationService')
+  }
+
   handleError(message: string) {
     this.notify$.next({ severity: 'error', message })
+    this.logger.error('Error message added', 'NotificationService')
   }
 
   handleSuccess(message: string) {
     this.notify$.next({ severity: 'success', message })
+    this.logger.info('Success message added', 'NotificationService')
   }
 
   clear() {
     this.notify$.next(null)
+    this.logger.warn('Notification cleared', 'NotificationService')
   }
 }
