ad48c54de9eb9fc0784986371fdfe1abe5b57bbf
Al1erEgo <al1erego@gmail.com>
2023-07-13 15:03:53 +0200

    feat: add logs in to tasks.component

--- src/app/todos/components/todos/todo/tasks/tasks.component.ts
+++ src/app/todos/components/todos/todo/tasks/tasks.component.ts
@@ -5,6 +5,7 @@ import { Task, UpdateTaskRequest } from 'src/app/todos/models/tasks.models'
 import { map } from 'rxjs/operators'
 import { TodosService } from 'src/app/todos/services/todos.service'
 import { TaskStatusEnum } from 'src/app/core/enums/taskStatus.enum'
+import {LoggerService} from "../../../../../shared/services/logger.service";
 
 @Component({
   selector: 'tl-tasks',
@@ -15,7 +16,9 @@ export class TasksComponent implements OnInit {
   @Input() todoId!: string
   tasks$?: Observable<Task[]>
   taskTitle = ''
-  constructor(private tasksService: TasksService, private todosService: TodosService) {}
+  constructor(private tasksService: TasksService, private todosService: TodosService, private logger: LoggerService) {
+    this.logger.info('Tasks component initialised', 'TasksComponent');
+  }
 
   ngOnInit(): void {
     //subscribe
@@ -35,18 +38,22 @@ export class TasksComponent implements OnInit {
       })
     )
     this.tasksService.getTasks(this.todoId)
+    this.logger.info('Tasks data initialized', 'TasksComponent');
   }
 
   addTaskHandler() {
+    this.logger.info('Add Task w/title:', 'TasksComponent', this.taskTitle);
     this.tasksService.addTask(this.todoId, this.taskTitle)
     this.taskTitle = ''
   }
 
   deleteTask(taskId: string) {
     this.tasksService.deleteTask(this.todoId, taskId)
+    this.logger.warn('Remove task w/id:', 'TasksComponent', taskId);
   }
 
   changeTaskStatus(event: { taskId: string; newTask: UpdateTaskRequest }) {
     this.tasksService.updateTask(this.todoId, event.taskId, event.newTask)
+    this.logger.warn('Update task(id) status to:', 'TasksComponent', {id: event.taskId, status: event.newTask.status});
   }
 }
