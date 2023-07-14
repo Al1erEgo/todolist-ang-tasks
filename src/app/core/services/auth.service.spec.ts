2e660ef19ffa83407f104ef09fd1d81fbf00cfc1
Al1erEgo <al1erego@gmail.com>
2023-07-14 13:26:23 +0200

    feat: creating tests on auth.service st2

--- src/app/core/services/auth.service.spec.ts
+++ src/app/core/services/auth.service.spec.ts
@@ -4,6 +4,9 @@ import {NotificationService} from "./notification.service";
 import {LoggerService} from "../../shared/services/logger.service";
 import {TestBed} from "@angular/core/testing";
 import {Router} from "@angular/router";
+import {CommonResponseType} from "../models/core.models";
+import {ResultCodeEnum} from "../enums/resultCode.enum";
+import {environment} from "../../../environments/environment.prod";
 
 describe('AuthService', ()=>{
   let service: AuthService
@@ -14,7 +17,10 @@ describe('AuthService', ()=>{
 
   const fakeNotificationService = jasmine.createSpyObj('NotificationService',['handleError', 'handleSuccess', 'clear'])
   const fakeLoggerService = jasmine.createSpyObj('LoggerService', ['info', 'error', 'warn'])
-  const fakeRouter = jasmine.createSpyObj('Router', ['navigate'])
+
+  class FakeRouter {
+    navigate = jasmine.createSpy('navigate');
+  }
 
   beforeEach(()=>{
     TestBed.configureTestingModule({
@@ -22,7 +28,7 @@ describe('AuthService', ()=>{
         providers: [AuthService,
           {provide: NotificationService, useValue: fakeNotificationService},
           {provide: LoggerService, useValue: fakeLoggerService},
-          {provide: Router, useValue: fakeRouter},
+          {provide: Router, useClass: FakeRouter},
         ]
     })
     service = TestBed.inject(AuthService);
@@ -39,6 +45,50 @@ describe('AuthService', ()=>{
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  describe('login', ()=>{
+    const loginData = { email: 'testuser@mail.com', password: 'password', rememberMe: false };
+
+    it('should send login request and navigate to home page if login is successful', ()=>{
+      const response: CommonResponseType<{ userId: number }> = {
+        resultCode: ResultCodeEnum.success,
+        data: { userId: 1 },
+        messages: []
+      };
+      service.login(loginData);
+      const req = httpMock.expectOne(`${environment.baseUrl}/auth/login`);
+      expect(req.request.method).toBe('POST');
+      req.flush(response);
+
+      expect(service.isAuth).toBeTrue();
+      expect(routerService.navigate).toHaveBeenCalledWith(['/']);
+      expect(notificationService.handleError).not.toHaveBeenCalled();
+      expect(loggerService.info).toHaveBeenCalledWith('Login request sent', 'AuthService');
+      expect(loggerService.info).toHaveBeenCalledWith('Login successful', 'AuthService');
+      expect(loggerService.error).not.toHaveBeenCalled();
+    })
+
+    it('should send login request and show error notification if login is unsuccessful', () => {
+      const response: CommonResponseType<any> = {
+        resultCode: ResultCodeEnum.error,
+        data: {},
+        messages: ["Incorrect Email or Password"],
+      };
+
+      service.login(loginData);
+      const req = httpMock.expectOne(`${environment.baseUrl}/auth/login`);
+      expect(req.request.method).toBe('POST');
+      req.flush(response);
+
+
+      expect(service.isAuth).toBeFalse();
+      expect(routerService.navigate).not.toHaveBeenCalled();
+      expect(notificationService.handleError).toHaveBeenCalledWith("Incorrect Email or Password");
+      expect(loggerService.info).toHaveBeenCalledWith('Login request sent', 'AuthService');
+      expect(loggerService.info).not.toHaveBeenCalledWith('Login successful', 'AuthService');
+      expect(loggerService.error).toHaveBeenCalledWith('Login request error:', 'AuthService', response);
+    });
+  })
 })
 
 
