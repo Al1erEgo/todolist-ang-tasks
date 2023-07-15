41559e8d511870cb9d4df2ab18c86de939b9becd
Al1erEgo <al1erego@gmail.com>
2023-07-15 17:32:31 +0200

    feat: cover todo-filter.component with tests

---
+++ src/app/todos/components/todos/todo/todo-filters/todo-filter.component.spec.ts
@@ -0,0 +1,33 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {TodoFiltersComponent} from "./todo-filters.component";
+import {FilterType} from "../../../../models/todos.models";
+
+
+describe('TodoFiltersComponent', () => {
+  let component: TodoFiltersComponent;
+  let fixture: ComponentFixture<TodoFiltersComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [TodoFiltersComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(TodoFiltersComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('Should be created', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('Should emit changeFilterEvent with when changeFilterHandler is called', () => {
+    const filter: FilterType = 'all';
+    spyOn(component.changeFilterEvent, 'emit');
+
+    component.changeFilterHandler(filter);
+
+    expect(component.changeFilterEvent.emit).toHaveBeenCalledWith(filter);
+  });
+
+});
